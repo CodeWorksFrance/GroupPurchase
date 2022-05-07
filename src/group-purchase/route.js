1976b176ea240a3d01ae7cfa6a471ee85f247208
Christophe Thibaut <cthibaut@octo.com>
2022-05-07 23:12:29 +0200

    uploading a csv file results in the converted purchase object

--- src/group-purchase/route.js
+++ src/group-purchase/route.js
@@ -31,17 +31,18 @@ app.post('/upload', (request, response) => {
             if (err)
                 return response.status(500).send(err)
         })
-        const parser = csvParse.parse({ delimiter: ',', columns: true, trim:true })
-        const records = []
+        const parser = csvParse.parse({delimiter: ',', columns: true, trim: true})
+        const items = []
         parser.on('readable', () => {
             let record;
-            while((record = parser.read()) !== null) {
+            while ((record = parser.read()) !== null) {
                 const purchaseItem = {
                     label: record.item,
                     unitPrice: Number.parseFloat(record.unitp),
                     quantity: Number.parseInt(record.qty),
-                    buyer: record.buyer }
-                records.push(purchaseItem)
+                    buyer: record.buyer
+                }
+                items.push(purchaseItem)
             }
         })
         parser.on('error', (err) => {
@@ -49,8 +50,14 @@ app.post('/upload', (request, response) => {
         })
         parser.write(purchaseFile.data);
         parser.end()
-        console.log(records)
-        response.send('File uploaded')
+        const purchase = {
+            user: request.body.user,
+            purchaseDate: request.body.date,
+            shippingFee: request.body.shippingFee,
+            items:items,
+        }
+        console.log("%j",purchase)
+        response.json(purchase)
     }
 })
 
