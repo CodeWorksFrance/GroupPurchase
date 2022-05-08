f7be89ba58eb0c2bc0f38c54dc7338aad6863b9e
Christophe Thibaut <cthibaut@octo.com>
2022-05-08 19:17:01 +0200

    first stab at purchase page

--- src/group-purchase/route.js
+++ src/group-purchase/route.js
@@ -20,8 +20,51 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(fileUpload({ createParentPath: true }))
 
 app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
+    response.render("purchase", { numbers: [42,17,4807,23,51] })
 })
+
+app.get('/purchase/:id', (request, response) => {
+    const id = parseInt(request.params.id)
+    pool.query('SELECT p.id, u.name, creation_date, shipping_fee FROM Purchases p INNER JOIN Users u ON u.id = p.user_id WHERE p.id = $1', [id], (error, result) => {
+        if (error || result.rows.length === 0) {
+            response.status(400).send(error)
+        } else {
+            const row = result.rows[0]
+            const purchase = {
+                id: row.id,
+                user: row.name,
+                creationDate: row.creation_date,
+                shippingFee: row.shipping_fee,
+                items: [],
+            }
+            pool.query('SELECT i.label, i.quantity, i.unit_price, u.name FROM Purchase_Items i INNER JOIN Users u ON u.id = i.buyer_id WHERE i.purchase_id = $1', [id], (error, result) => {
+                if (error) {
+                    response.status(400).send(error)
+                } else {
+                    result.rows.forEach(row => {
+                        const item = {
+                            label: row.label,
+                            quantity: row.quantity,
+                            unitPrice: row.unit_price,
+                            amount: row.quantity * row.unit_price,
+                            buyer: row.name,
+                        }
+                        purchase.items.push(item)
+                    })
+                    response.render("purchase", { purchase: purchase, getTotal: (items) => {
+                            let result = 0;
+                            items.forEach(item => {
+                                result += item.amount
+                            })
+                            return result
+                        }
+                    })
+                }
+            })
+        }
+    })
+})
+
 app.get('/new', (request, response) => {
     response.render("new-group-purchase")
 })
