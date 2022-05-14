0f8534409907d65c6d5475c3f4d72d196c6e6853
Christophe Thibaut <cthibaut@octo.com>
2022-05-14 21:51:13 +0200

    computeBills 1 bill for 1 item and 1 buyer

--- src/domain/computeBills.js
+++ src/domain/computeBills.js
@@ -1,5 +1,9 @@
 const computeBills = (purchase) => {
-    return []
+    const item = purchase.items[0]
+    const amount = item.unitPrice * item.quantity
+    const total = amount + purchase.shippingFee
+    const bill = { buyer: item.buyer, amount: amount, shipping: purchase.shippingFee, total: total }
+    return [bill]
 }
 
 module.exports = computeBills
--- test/domain/computeBills.spec.js
+++ test/domain/computeBills.spec.js
@@ -1,12 +1,9 @@
 const computeBills = require('../../src/domain/computeBills')
 
 describe('computeBills', () => {
-    it('should create one bill for a purchase by one person', () => {
+    it('should create one bill for a purchase of one item by one person', () => {
         const purchase = { shippingFee: 10.0, items: [
-                { label: "paper", unitPrice: 4.5, quantity: 10, buyer: 'Clara' },
-                { label: "pencils", unitPrice: 2, quantity: 5, buyer: 'Clara' }] }
-        result = computeBills(purchase)
-        expected = [{ buyer: 'Clara', amount: 45.0 + 10.0, shipping: 10.0, buyer: 'Clara'}]
-        expect(result).toBe(expected)
+                { label: "paper", unitPrice: 4.5, quantity: 10, buyer: 'Clara' }]}
+        expect(computeBills(purchase)).toStrictEqual([{ buyer: 'Clara', amount: 45.0, shipping: 10.0, total: 55.0}])
     })
 })
