5b0e77451f2f643ada3a6fabdd54ef136c2927d8
Christophe Thibaut <cthibaut@octo.com>
2022-05-15 09:36:45 +0200

    importItems should parse csv data and produce items

---
+++ test/domain/importItems.spec.js
@@ -0,0 +1,16 @@
+const importItems = require('../../src/domain/importItems')
+
+describe( 'importItems', () =>
+{
+    it('should parse csv data and produce items', () => {
+        const csvData =
+            `item              , unitp  , qty , amount , buyer
+    pencils           ,   0.10 ,  10 ,  1.00 , Bertrand
+    paper             ,   0.15 ,   8 ,  1.00 , Clara
+    rubber            ,   0.10 ,  10 ,  1.00 , Alice `
+        expect(importItems(csvData)).toStrictEqual([
+            {label: 'pencils', unitPrice: 0.10, quantity: 10, buyer: 'Bertrand'},
+            {label: 'paper', unitPrice: 0.15, quantity: 8, buyer: 'Clara'},
+            {label: 'rubber', unitPrice: 0.10, quantity: 10, buyer: 'Alice'}])
+    })
+})
