2f93227aae17697ba48abf944dc85799231fbb52
Michelle AVOMO <michelle.avomo@codeworks.fr>
2022-06-08 16:38:32 +0200

    init

--- src/group-purchase/route.js
+++ src/group-purchase/route.js
@@ -120,7 +120,7 @@ app.post('/upload', (request, response) => {
                     items: items,
                 }
                 createPurchase(purchase, response)
-                console.log("%j", purchase)
+                //console.log("%j", purchase)
             }
         })
     }
@@ -132,18 +132,22 @@ app.listen(port, () => {
 
 const createPurchase = (purchase, response) => {
     var purchaseId;
+  //  console.log("createPurchase:\n%j\n", purchase)
+   // console.log([purchase.user, purchase.purchaseDate, purchase.shippingFee])
     pool.query('INSERT INTO PURCHASES (User_Id, Creation_Date, Shipping_Fee) SELECT u.Id, $2, $3 FROM Users as u WHERE u.name = $1 RETURNING ID;',
         [purchase.user, purchase.purchaseDate, purchase.shippingFee], (error, result) => {
             if (error) {
-                console.log(error)
+                //console.log(error)
                 response.status(400).send(error)
             } else {
                 let purchaseId = result.rows[0].id;
+                //console.log("PURCHASE#:\n%d\n", purchaseId)
                 for (let i = 0; i < purchase.items.length; i++) {
+                  //  console.log([purchaseId, purchase.items[i].label, purchase.items[i].quantity, purchase.items[i].unitPrice, purchase.items[i].buyer])
                     pool.query('INSERT INTO PURCHASE_ITEMS(Purchase_Id, Label, Quantity, Unit_Price, Buyer_Id) SELECT $1, $2, $3, $4, u.Id FROM Users AS u WHERE u.name = $5;',
                         [purchaseId, purchase.items[i].label, purchase.items[i].quantity, purchase.items[i].unitPrice, purchase.items[i].buyer], (error, result) => {
                             if (error) {
-                                console.log(error)
+                                //console.log(error)
                                 response.status(400).send(error)
                                 return
                             }
